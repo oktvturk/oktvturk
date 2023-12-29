147c67b93aa7702c58530c76e919e8a2cb417607
oktvturk <155159710+oktvturk@users.noreply.github.com>
2023-12-29 13:12:58 +0100

    Add files via upload

---
+++ links.ts
@@ -0,0 +1,118 @@
+
+export const streams = [
+	{
+		key: 'kanald',
+		src: 'https://demiroren-live.daioncdn.net/kanald/kanald.m3u8',
+		pic: 'pics/kanald.svg'
+	},
+	{
+		key: 'star',
+		src: 'https://dogus-live.daioncdn.net/startv/startv_720p.m3u8',
+		pic: 'pics/star.svg'
+	},
+	{
+		key: 'show',
+		src: 'https://ciner-live.daioncdn.net/showtv/showtv.m3u8',
+		pic: 'pics/show.svg'
+	},
+	{
+		key: 'kanal7',
+		src: 'https://kanal7-live.daioncdn.net/kanal7/kanal7_1080p.m3u8',
+		pic: 'pics/kanal7.png'
+	},
+	{
+		key: 'fox',
+		src: 'https://foxtv.blutv.com/blutv_foxtv_live/live.m3u8',
+		pic: 'pics/fox.svg'
+	},
+	{
+		key: 'ntv',
+		src: 'https://dogus-live.daioncdn.net/ntv/ntv.m3u8',
+		pic: 'pics/ntv.png'
+	},
+	{
+		key: 'haberturk',
+		src: 'https://ciner-live.daioncdn.net/haberturktv/haberturktv_1080p.m3u8',
+		pic: 'pics/haberturk.png'
+	},
+	{
+		key: '360tv',
+		src: 'https://turkmedya-live.ercdn.net/tv360/tv360.m3u8',
+		pic: 'pics/360tv.png'
+	},
+	{
+		key: 'trtbelgesel',
+		src: 'https://tv-trtbelgesel.medya.trt.com.tr/master_720.m3u8',
+		pic: 'pics/trtbelge.svg'
+	},
+	{
+		key: 'trtcocuk',
+		src: 'https://tv-trtcocuk.live.trt.com.tr/master.m3u8',
+		pic: 'pics/trtcocuk.png'
+	},
+	{
+		key: 'cine1',
+		src: 'https://live.artidijitalmedya.com/artidijital_cine1/cine1/playlist.m3u8',
+		pic: 'pics/cine1.jpeg'
+	},
+	{
+		key: 'meclistv',
+		src: 'https://meclistv-live.ercdn.net/meclistv/meclistv_720p.m3u8?e=&st=',
+		pic: 'pics/tbmm.png'
+	},
+	{
+		key: 'cnnturk',
+		src: 'https://live.duhnet.tv/S2/HLS_LIVE/cnnturknp/playlist.m3u8',
+		pic: 'pics/cnnturk.png'
+	},
+	{
+		key: 'ulusaltv',
+		src: 'https://stream.guventechnology.com:19360/ulusaltv/ulusaltv.m3u8',
+		pic: 'pics/ulusal.png'
+	},
+	{
+		key: 'mkaradeniz',
+		src: 'https://live.artidijitalmedya.com/artidijital_mavikaradeniz/mavikaradeniz/playlist.m3u8',
+		pic: 'pics/mkdeniz.jpg'
+	},
+	{
+		key: 'kraltvpop',
+		src: 'https://dogus-live.daioncdn.net/kralpoptv/playlist.m3u8',
+		pic: 'pics/kralpop.png'
+	},
+	{
+		key: 'trt1',
+		src: 'https://tv-trt1-dai.medya.trt.com.tr/master_1.m3u8',
+		pic: 'pics/trt1.svg'
+	},
+	{
+		key: 'trtbelgesel',
+		src: 'https://tv-trtbelgesel.medya.trt.com.tr/master_720.m3u8',
+		pic: 'pics/trtbelge.svg'
+	},
+	{
+		key: 'trthaber',
+		src: 'https://tv-trthaber.medya.trt.com.tr/master_720.m3u8',
+		pic: 'pics/trthaber.svg'
+	},
+	{
+		key: 'trtturk',
+		src: 'https://tv-trtturk.medya.trt.com.tr/master.m3u8',
+		pic: 'pics/trtturk.svg'
+	},
+	{
+		key: 'trtworld',
+		src: 'https://tv-trtworld.live.trt.com.tr/master.m3u8',
+		pic: 'pics/trtworld.png'
+	},
+	{
+		key: 'kanal24',
+		src: 'https://mn-nl.mncdn.com/kanal24/smil:kanal24.smil/playlist.m3u8',
+		pic: 'pics/kanal24.png'
+	},
+	{
+		key: 'dreamtv',
+		src: 'https://live.duhnet.tv/S2/HLS_LIVE/dreamturknp/playlist.m3u8',
+		pic: 'pics/dreamtv.png'
+	},
+]
